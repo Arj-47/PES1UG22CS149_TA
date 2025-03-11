cf354c04419d3c103e29d5b5bf9c313134f17ae9
Arj-47 <arjunsomanna10@gmail.com>
2025-03-11 15:58:46 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,8 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Hello, World 2\n";
-  cout << "Hello, Jenkins 2\n";
-  cout << "I have successfully built and run this 2  \n";
+  cout << "Hello, World PES1UG22CS149\n";
+  
   return 0;
 }
